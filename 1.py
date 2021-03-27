00e00a6b7c5f7c5c0efd4658f378087c1baf062b
newaccountforGit <newmailfortotfip@mail.ru>
2021-03-27 10:44:16 +0300

    init commit

---
+++ 1.py
@@ -0,0 +1,10 @@
+import datetime
+import re
+
+            
+def main():
+    
+    pass
+            
+if __name__ == "__main__":
+    main()
---
+++ 2.py
@@ -0,0 +1,9 @@
+import datetime
+
+            
+def main():
+    
+    pass
+            
+if __name__ == "__main__":
+    main()
